3316950ec3335ce5a2dde09843bd85d0fe778ad6
Megan McBride <meganambermcbride@gmail.com>
2023-07-01 11:03:27 -0400

    Fix footer and navbar static

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
 
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 function App() {
   return (
     <BrowserRouter>
+    <div className="main">
       <Navbar />
       <div className="layout-container">
       <Sidebar />
@@ -21,6 +24,8 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
       </div>
+      <Footer />
+    </div>
     </BrowserRouter>
   );
 }
