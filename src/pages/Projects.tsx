353def61fc09c74da6ca3ab6aa05c2ca73810ea7
Megan McBride <meganambermcbride@gmail.com>
2023-07-03 22:24:10 -0400

    Remove CSS and fix project layout issue

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,13 +1,36 @@
 import React from "react";
+import tunesquad from "../images/tunesquad.gif";
+
 
 function Projects() {
   return (
     <>
+  
+    <div className="projects-container">
+      <h1>Projects</h1>
+      <div className="projects-layout"> 
+
+      <div className="projects-image">placeholder</div>
+      {/* <img src={tunesquad} alt="tunesquad" /> */}
       <div className="projects-button-container">
         <a className="projects-button">
           <span className="projects-number">1</span> TuneSquad
         </a>
+        <a className="projects-button">
+          <span className="projects-number">2</span> LooseLeaf
+        </a>
+        <a className="projects-button">
+          <span className="projects-number">3</span> Tweeter
+        </a>
+        <a className="projects-button">
+          <span className="projects-number">4</span> Scheduler
+        </a>
+        <a className="projects-button">
+          <span className="projects-number">5</span> Jungle
+        </a>
       </div>
+      </div>
+    </div>
     </>
   );
 }
